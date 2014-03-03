5ddf08abf9bb7ad0cfef2ca4c1e805642bf5f3e9
James Booth <boothj5@gmail.com>
2014-03-03 00:19:13 +0000

    More big graph tests

--- tests/graphs/BigGraphTest.java
+++ tests/graphs/BigGraphTest.java
@@ -186,4 +186,75 @@ public class BigGraphTest {
 		assertTrue(ints.contains("int1"));
 		assertTrue(ints.contains("int2"));
 	}
+
+	@Test
+	public void test9() throws Exception {
+		//given
+		RoleMappingProcessor rmp = new RoleMappingProcessor("rules/big-graph.xml", null);
+		rmp.loadRules();
+		Set<String> props = new HashSet<String>();
+		props.add("prop1");
+		props.add("prop5");
+		
+		// when
+		List<String> ints = rmp.getInts(props);
+		
+		// then
+		assertEquals(1, ints.size());
+		assertTrue(ints.contains("int1"));
+	}
+
+	@Test
+	public void test10() throws Exception {
+		//given
+		RoleMappingProcessor rmp = new RoleMappingProcessor("rules/big-graph.xml", null);
+		rmp.loadRules();
+		Set<String> props = new HashSet<String>();
+		props.add("prop1");
+		props.add("prop6");
+		
+		// when
+		List<String> ints = rmp.getInts(props);
+		
+		// then
+		assertEquals(1, ints.size());
+		assertTrue(ints.contains("int1"));
+	}
+
+	@Test
+	public void test11() throws Exception {
+		//given
+		RoleMappingProcessor rmp = new RoleMappingProcessor("rules/big-graph.xml", null);
+		rmp.loadRules();
+		Set<String> props = new HashSet<String>();
+		props.add("prop1");
+		props.add("prop5");
+		props.add("prop6");
+		
+		// when
+		List<String> ints = rmp.getInts(props);
+		
+		// then
+		assertEquals(2, ints.size());
+		assertTrue(ints.contains("int1"));
+		assertTrue(ints.contains("int3"));
+	}
+
+	@Test
+	public void test12() throws Exception {
+		//given
+		RoleMappingProcessor rmp = new RoleMappingProcessor("rules/big-graph.xml", null);
+		rmp.loadRules();
+		Set<String> props = new HashSet<String>();
+		props.add("prop2");
+		props.add("prop3");
+		
+		// when
+		List<String> ints = rmp.getInts(props);
+		
+		// then
+		assertEquals(2, ints.size());
+		assertTrue(ints.contains("int1"));
+		assertTrue(ints.contains("int2"));
+	}
 }
